98d96d5d60a77ee046120655bc10bfef57690beb
dcpomichter <dcpomichter@gmail.com>
2025-11-23 21:51:53 -0500

    environment updates

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    MONGO_URI: process.env.MONGO_URI,
+    TOKEN_SECRET: process.env.TOKEN_SECRET,
+    DOMAIN: process.env.DOMAIN,
+    MAILER_USER: process.env.MAILER_USER,
+    MAILER_PASS: process.env.MAILER_PASS,
+    NEXT_PUBLIC_MAP_URL: process.env.NEXT_PUBLIC_MAP_URL,
+    NEXT_PUBLIC_API_KEY: process.env.NEXT_PUBLIC_API_KEY
+  }
 };
 
 export default nextConfig;
