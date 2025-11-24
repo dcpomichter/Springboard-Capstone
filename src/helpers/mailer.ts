ce73f24c7dff01bd0ee110aed152bd2be142a25e
dcpomichter <dcpomichter@gmail.com>
2025-11-23 23:52:01 -0500

    updated mailer

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -1,11 +1,12 @@
-import Mailjet from 'node-mailjet';
 import User from '@/models/userModel';
 import bcrypt from 'bcryptjs';
+import { Client, SendEmailV3_1, LibraryResponse } from 'node-mailjet';
 
-const mailjet = new Mailjet({
+const mailjet = new Client({
     apiKey: process.env.MJ_APIKEY_PUBLIC,
     apiSecret: process.env.MJ_APIKEY_PRIVATE
 });
+
 
 export const sendEmail = async ({ email, emailType, userId }: any) => {
     try {
@@ -23,29 +24,41 @@ export const sendEmail = async ({ email, emailType, userId }: any) => {
             }, { new: true, runValidators: true })
         }
 
+        const data: SendEmailV3_1.Body = {
+            Messages: [
+                {
+                    From: {
+                        Email: "admin@boardbums.vercel.app",
+                    },
+                    To: [
+                        {
+                            Email: email,
+                        },
+                    ],
+                    TemplateErrorReporting: {
+                        Email: 'reporter@boardbums.vercel.app',
+                        Name: 'Reporter',
+                    },
+                    Subject: emailType === "VERIFY" ? "Verify your Email" : "Reset your Password",
+                    HTMLPart: emailType === "VERIFY" ?
+                        `<p>Click <a href="${process.env.DOMAIN}/verifyemail?token=${hashedToken}">here</a> to Verify your Email or copy and paste the link below into your browser. <br> ${process.env.DOMAIN}/verifyemail?token=${hashedToken}</p>`
+                        :
+                        `<p>Click <a href="${process.env.DOMAIN}/reset?token=${hashedToken}">here</a> to Reset your Password or copy and paste the link below into your browser. <br> ${process.env.DOMAIN}/reset?token=${hashedToken}</p>`,
+                    TextPart: emailType === "VERIFY" ?
+                        `Click here to Verify your Email or copy and paste the link below into your browser. ${process.env.DOMAIN}/verifyemail?token=${hashedToken}`
+                        :
+                        `Click here to Reset your Password or copy and paste the link below into your browser. ${process.env.DOMAIN}/reset?token=${hashedToken}`,
+                },
+            ],
+        };
 
-        const transport = nodemailer.createTransport({
-            host: "sandbox.smtp.mailtrap.io",
-            port: 2525,
-            auth: {
-                user: `${process.env.MAILER_USER}`,
-                pass: `${process.env.MAILER_PASS}`
-            }
-        });
+        const result: LibraryResponse<SendEmailV3_1.Response> = await mailjet
+            .post('send', { version: 'v3.1' })
+            .request(data);
 
-        const mailOptions = {
-            from: "admin@boardbums.com",
-            to: email,
-            subject: emailType === "VERIFY" ? "Verify your Email" : "Reset your Password",
-            html: emailType === "VERIFY" ?
-                `<p>Click <a href="${process.env.DOMAIN}/verifyemail?token=${hashedToken}">here</a> to Verify your Email or copy and paste the link below into your browser. <br> ${process.env.DOMAIN}/verifyemail?token=${hashedToken}</p>`
-                :
-                `<p>Click <a href="${process.env.DOMAIN}/reset?token=${hashedToken}">here</a> to Reset your Password or copy and paste the link below into your browser. <br> ${process.env.DOMAIN}/reset?token=${hashedToken}</p>`
-        }
+        const { Status } = result.body.Messages[0];
 
-        const mailResponse = await transport.sendMail(mailOptions)
-
-        return mailResponse
+        return result
 
     }
     catch (error: any) {
